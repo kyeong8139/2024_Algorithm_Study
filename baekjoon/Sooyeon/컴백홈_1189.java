0ae78b703020264ec3208a98ca00920fd71541b4
5ooyeon <symelcho@inu.ac.kr>
2024-02-02 00:14:13 +0900

    [baekjoon_1189] 컴백홈

--- baekjoon/Sooyeon/컴백홈_1189.java
+++ baekjoon/Sooyeon/컴백홈_1189.java
@@ -1,45 +1,61 @@
 import java.util.Scanner;
 
 public class 컴백홈_1189 {
-    static char[][] arr = null;
     static int routeCnt = 0;
     static int ansCnt = 0;
 
-    static int[][] routeSave = null;
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
+        String[] temp = sc.nextLine().split(" ");
+        int n = Integer.parseInt(temp[0]);
+        int m = Integer.parseInt(temp[1]);
 
-        routeCnt= sc.nextInt();
+        routeCnt= Integer.parseInt(temp[2]);
 
-        arr = new char[n][m];
-        routeSave = new int[n][m];
+        char[][] routeSave = new char[n][m];
+        // int[][] routeSave = new int[n][m];
+
 
         for(int i=0;i<n;i++) {
-            arr[n] = sc.nextLine().toCharArray();
+            routeSave[i] = sc.nextLine().toCharArray();
         }
 
+        getRoute(n-1, 0, routeSave, 1);
+        System.out.println(ansCnt);
     }
 
-    // //(n-1, 0)에서 (0, m-1)까지 cnt로 가는 가지수 
-    // static int getRoute(int x, int y, int cnt) {
+    //(n-1, 0)에서 (0, m-1)까지 cnt로 가는 가지수
+    static void getRoute(int x, int y, char[][] routeSave, int cnt) {
 
-    //     //x,y, cnt가 음수이거나 나올 수 없는 수라면 종료
-    //     while(x>0 && x<arr.length && y>=0 && y<arr[0].length && cnt>0) {
-            
-            
+        if (x == 0 && y == routeSave[0].length - 1 && cnt == routeCnt) { // 목적지 도달 및 거리가 맞는지 확인
+            ++ansCnt;
+            return; // 경로가 완성되었으므로 종료
+        }
 
-    //         //간 곳에 장애물이 있다면 함수 종료
-    //         if(arr[x][y]=='T') {
-    //             return 0;
-    //         }
-    //     }
+        // 온 좌표 T로 바꾸기
+        routeSave[x][y] = 'T';
 
+        // 상
+        if (x - 1 >= 0 && routeSave[x - 1][y] != 'T') {
+            getRoute(x - 1, y, routeSave, cnt + 1);
+        }
 
+        // 하
+        if (x + 1 < routeSave.length && routeSave[x + 1][y] != 'T') {
+            getRoute(x + 1, y, routeSave, cnt + 1);
+        }
 
-    //     //저장된 값이 있다면
-    // }
+        // 좌
+        if (y - 1 >= 0 && routeSave[x][y - 1] != 'T') {
+            getRoute(x, y - 1, routeSave, cnt + 1);
+        }
 
+        // 우
+        if (y + 1 < routeSave[x].length && routeSave[x][y + 1] != 'T') {
+            getRoute(x, y + 1, routeSave, cnt + 1);
+        }
+
+        // 다시 원래 상태로 되돌리기.
+        routeSave[x][y] = '.';
+    }
 }
