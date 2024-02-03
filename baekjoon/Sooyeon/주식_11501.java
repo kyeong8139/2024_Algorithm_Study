b187e1f34482df473b9dee3937b9bf31e8754be5
5ooyeon <symelcho@inu.ac.kr>
2024-02-04 01:30:19 +0900

    [baekjoon_11501] 주식

--- baekjoon/Sooyeon/주식_11501.java
+++ baekjoon/Sooyeon/주식_11501.java
@@ -1,56 +1,40 @@
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class 주식_11501 {
     static int ans = 0;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = Integer.parseInt(sc.nextLine());
-        String[][] arr = new String[n][];
+        int n = sc.nextInt(); //test case 받기
 
         for(int i=0;i<n;i++) {
-            sc.nextLine();
-            arr[i] = sc.nextLine().split(" ");
-            ans = 0;
-            getMax(arr[i]);
+
+            Stack<Integer> st = new Stack<>(); //새로운 stack 생성
+            ans = 0; //정답 초기화
+
+            int k = sc.nextInt(); //stack에 넣을 횟수 받기
+
+            for(int j=0;j<k;j++) {
+                st.push(sc.nextInt());
+            }
+
+            getMoney(st, st.pop()); //getMoney함수에 stack과 가장 마지막값 넣어서 호출
             System.out.println(ans);
+
         }
-
-
 
     }
 
-    private static void getMax(String[] arr) { 
-        int[][] getIndexArr = new int[arr.length][2];
-        for(int i=0;i<getIndexArr.length;i++) {
-            getIndexArr[i][0] = Integer.parseInt(arr[i]);
-            getIndexArr[i][1] = i;
-        }
+    private static void getMoney(Stack<Integer> st, int max) {
+        while(!st.empty()) { //stack이 empty가 아닐때만 실행
+            int stInt = st.pop(); //stack에서 값을 받아와
         
-        Arrays.sort(getIndexArr, new Comparator<int[]>() {
-
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                return o2[0] - o1[0];
+            if(max < stInt) { //값이 max값 보다 크다면, max값을 바꿔주어 함수 다시 실행
+                getMoney(st, stInt);
+            } else { //max가 더 크다면 max값으로 주식을 팔고, 같은 함수 다시실행
+                ans += max - stInt;
+                getMoney(st, max);
             }
-            
-        });
-
-        if(getIndexArr[0][0] == Integer.parseInt(arr[0])) {
-            return;
-        }
-
-        for(int i=0;i<getIndexArr[0][1];i++) {
-            ans += getIndexArr[0][0] - Integer.parseInt(arr[i]);
-        }
-
-        if(getIndexArr[0][1]!=arr.length-1) {
-            String[] getBenefit = new String[arr.length-getIndexArr[0][1]-1];
-            for(int i=0;i<getBenefit.length;i++) {
-                getBenefit[i] = arr[i+getIndexArr[0][1]+1];
-            }
-            getMax(getBenefit);
         }
     }
 }
