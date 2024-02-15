23e394b0313c83005a67df761304df190716b5bc
jun-23 <78009502+jun-23@users.noreply.github.com>
2024-02-15 13:38:57 +0900

    [beakjoon-2579]계단오르기

---
+++ baekjoon/Heajun/계단오르기_2579.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class 계단오르기_2579 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] dp = new int[N+1];
+		int[] arr = new int[N+1];
+		for(int i = 1; i <= N; i++) {
+			arr[i] = sc.nextInt();
+		}
+		dp[1] = arr[1];
+		
+		if(N >= 2) {
+			dp[2] = dp[1] + arr[2];
+			for(int i = 3; i <= N; i++) {
+				dp[i] = Math.max(arr[i]+dp[i-2], arr[i] + arr[i-1] + dp[i-3]);
+			}
+		}
+		
+		System.out.println(dp[N]);
+	}
+}
