c2571b664cccf21dfa242f857772532fe4c14006
5ooyeon <melthetensai@gmail.com>
2024-03-28 23:23:36 +0900

    [baekjoon_1504] 특정한최단경로

--- baekjoon/Sooyeon/특정한최단경로_1504.java
+++ baekjoon/Sooyeon/특정한최단경로_1504.java
@@ -1,19 +1,25 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 import java.util.TreeSet;
 
 public class 특정한최단경로_1504 {
 
-    static class Node{
-        int a, b, value;
+    static class Node implements Comparable<Node>{
+        int b, value;
 
-        Node(int a, int b, int value) {
-            this.a = a;
+        Node(int b, int value) {
             this.b = b;
             this.value = value;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return this.value - o.value;
         }
     }
 
@@ -25,37 +31,37 @@ public class 특정한최단경로_1504 {
 
         int N =Integer.parseInt(st.nextToken()), E = Integer.parseInt(st.nextToken());
 
-        TreeSet<Node> set = new TreeSet<>(new Comparator<Node>() {
+        ArrayList<Node>[] pq = new ArrayList[N];
 
-            @Override
-            public int compare(Node o1, Node o2) {
-                if(o1.value == o2.value) {
-                    return -1;
-                }
-                return o1.value - o2.value;
-            }
-            
-        });
+        for(int i=0;i<N;i++) {
+            pq[i] = new ArrayList<>();
+        }
 
         for(int i=0;i<E;i++) {
             st = new StringTokenizer(bf.readLine());
-            set.add(new Node(Integer.parseInt(st.nextToken())-1, Integer.parseInt(st.nextToken())-1, Integer.parseInt(st.nextToken())));
+            int a = Integer.parseInt(st.nextToken())-1,
+            b = Integer.parseInt(st.nextToken())-1,
+            c = Integer.parseInt(st.nextToken());
+
+            pq[a].add(new Node(b, c));
+
         }
 
         arr = new int[N];
         for(int i=0;i<N;i++) {
-            arr[i] = i;
+            arr[i] = Integer.MAX_VALUE;
         }
 
-        st = new StringTokenizer(bf.readLine());
-        int a = Integer.parseInt(st.nextToken())-1, b = Integer.parseInt(st.nextToken())-1;
-        arr[Math.max(a, b)] = Math.min(a, b);
+        arr[0] = 0;
 
-        while(true) {
-            Node n = set.pollFirst();
-            a = find(n.a); b = find(n.b);
-            if(a != b) {
-                arr[Math.max(a, b)] = Math.min(a, b);
+        PriorityQueue<Node> dij = new PriorityQueue<>();
+        dij.offer(new Node(0, 0));
+        while(dij!=null) {
+            Node n = dij.poll();
+            int idx = n.b, value = n.value;
+
+            for(Node node : pq[idx]) {
+                if(arr[n])
             }
         }
 
