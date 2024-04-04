d7a168453ba0429721933da838c67f0802386760
5ooyeon <symelcho@inu.ac.kr>
2024-04-05 00:24:21 +0900

    [baekjoon_1967] 트리의 지름

--- baekjoon/Sooyeon/트리의지름_1967.java
+++ baekjoon/Sooyeon/트리의지름_1967.java
@@ -1,22 +1,18 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class 트리의지름_1967 {
-    static class Temp {
-        int b, value;
+    static class Route {
+        int edge, value;
 
-        Temp(int b, int value) {
-            this.b = b;
+        Route(int edge, int value) {
+            this.edge = edge;
             this.value = value;
         }
     }
     static int max;
     static int arr[][];
-    static ArrayList<Temp> list[];
+    static ArrayList<Route> list[];
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(bf.readLine());
@@ -29,45 +25,38 @@ public class 트리의지름_1967 {
 
         for(int i=1;i<N;i++) {
             StringTokenizer st = new StringTokenizer(bf.readLine());
-            int a = Integer.parseInt(st.nextToken())-1, b = Integer.parseInt(st.nextToken())-1, c = Integer.parseInt(st.nextToken());
+            int startEdge = Integer.parseInt(st.nextToken())-1,
+             endEdge = Integer.parseInt(st.nextToken())-1,
+             value = Integer.parseInt(st.nextToken());
 
-            list[a].add(new Temp(b, c));
+             //트리의 부모를 index로 하는 list 내부에 자식노드와 가중치를 갖는 Route를 넣어주기.
+            list[startEdge].add(new Route(endEdge, value));
         }
         max = 0;
         getLength(0);
         System.out.println(max);
         
-    }  
-    static int getLength(int num) {
+    }
 
-        int maxValue = Integer.MIN_VALUE;
-        int maxNode = -1;
-
-        for(Temp t: list[num]) {
-            if(t.value > maxValue) {
-                maxValue = t.value; maxNode = t.b;
-            }
+    static int getLength(int edge) {
+        switch (list[edge].size()) {
+            case 0: //리프일경우 가중치가 없음
+                return 0;
+            case 1: //자식노드가 하나일경우
+                Route childNode = list[edge].get(0);
+                int childNodeLength = getLength(childNode.edge)+childNode.value;
+                // max는 계속해서 갱신
+                max = Math.max(childNodeLength, max);
+                return childNodeLength;
+            default: //두개 이상
+                PriorityQueue<Integer> que = new PriorityQueue<>((a, b)->b-a);
+                for(Route route : list[edge]){
+                    que.add(getLength(route.edge)+route.value);
+                }
+                int tmp1 = que.poll();
+                int tmp2 = que.poll();
+                max = Math.max(max, tmp1+tmp2);
+                return tmp1;
         }
-
-        if(maxNode == -1) {
-            return 0;
-        }
-        if(list[num].size()==1){
-            int aa = getLength(list[num].get(0).b)+list[num].get(0).value;
-            max = Math.max(aa, max);
-            return aa;
-        }
-        if(list[num].size()>=2){
-            PriorityQueue<Integer> que = new PriorityQueue<>((a, b)->b-a);
-            for(Temp t : list[num]){
-                que.add(getLength(t.b)+t.value);
-            }
-            int tmp1 = que.poll();
-            int tmp2 = que.poll();
-            max = Math.max(max, tmp1+tmp2);
-            return tmp1;
-
-        }
-        return 0;
     }
 }
