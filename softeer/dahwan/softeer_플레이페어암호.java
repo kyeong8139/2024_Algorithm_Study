18de4c4388a917deaad3de59194e2163ddbb472e
hwan-da <156666531+hwan-da@users.noreply.github.com>
2024-07-05 14:37:08 +0900

    Update softeer_플레이페어암호.java

--- softeer/dahwan/softeer_플레이페어암호.java
+++ softeer/dahwan/softeer_플레이페어암호.java
@@ -1,174 +1,120 @@
-package study2;
+import java.util.*;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+public class softeer_플레이페어 {
 
-public class softeer_플레이페어암호 {
-	
-	static char[] message;
-	static char[] key;
-	static char[][] playfair;
-	static int[][] index;
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		message = sc.next().toCharArray();
-		key = sc.next().toCharArray();
-		playfair = new char[5][5];
-		
-		boolean[] alpha = new boolean[26];
-		
-		// playfair 만들기
-		int idx = 0;
-		o : for(int r = 0; r < 5; r++) {
-			for(int c = 0; c < 5; c++) {
+    static class Node {
+        int r, c;
 
-				while(idx != key.length && alpha[key[idx] - 'A']) {
-					idx++;
-				}
-				if(idx == key.length) break o; 
-				
-				playfair[r][c] = key[idx];
-				alpha[key[idx] - 'A'] = true;
-				
-			}
-			
-		}
-		
-		for(int r = 0; r < 5; r++) {
-			for (int c = 0; c < 5; c++) {
-				if(playfair[r][c] == '\u0000') {
-					for(int i = 0; i < 26; i++) {
-						if(i == 9) continue;
-						if(!alpha[i]) playfair[r][c] = (char) (i + 'A');
-						break;
-					}
-				}
-			}
-		}
-		
-		// message 쪼개기
-		List<char[]> list = new ArrayList<>();
-		
-		int idx2 = 0;
-		while(idx2 < message.length) {
-			char[] temp = new char[2];
-			if(idx2 == message.length - 1) {
-				temp[0] = message[idx2];
-				temp[1] = 'X';
-				list.add(temp);
-				break;
-			}
-			
-			
-			if(message[idx2] == message[idx2 + 1]) {
-				if(message[idx2] == 'X') {
-					temp[0] = 'X';
-					temp[1] = 'Q';
-				} else {
-					temp[0] = message[idx2];
-					temp[1] = 'X';
-				}
-				idx2++;
-			} else {
-				temp[0] = message[idx2];
-				temp[1] = message[idx2 + 1];
-				idx2 += 2;
-			}
-			
-			list.add(temp);
-		}
-		
-		for(int i = 0; i < list.size(); i++) {
-			index = new int[2][2];
-			boolean isRow = false;
-			boolean isCol = false;
-			// 가로 체크
-			isRow = checkRow(list.get(i));
-			// 세로 체크
-			if(isRow) {
-				
-			} else {
-				isCol = checkCol(list.get(i));
-			}
-			
-		}
-		
-		
-		for(char[] i : list) {
-			System.out.println(Arrays.toString(i));
-		}
-		
-		
-	}
-	
-	public static boolean checkRow(char[] arr) {
-		
-		for(int r = 0; r < 5; r++) {
-			boolean flag1 = false;
-			boolean flag2 = false;
-			for(int c = 0; c < 5; c++) {
-				if(!flag2 && playfair[r][c] == arr[0]) {
-					index[0][0] = r;
-					index[0][1] = c;
-					flag1 = true;
-				}
-				else if(flag2 && playfair[r][c] == arr[0]) {
-					index[1][0] = r;
-					index[1][1] = c;
-					return true;
-				}
-				
-				if(!flag1 && playfair[r][c] == arr[0]) {
-					index[0][0] = r;
-					index[0][1] = c;
-					flag2 = true;
-				}
-				else if(flag1 && playfair[r][c] == arr[0]) {
-					index[1][0] = r;
-					index[1][1] = c;
-					return true;
-				}
-			}
-		}
-		
-		return false;
-	}
-	
-	public static boolean checkCol(char[] arr) {
-		
-		for(int c = 0; c < 5; c++) {
-			boolean flag1 = false;
-			boolean flag2 = false;
-			for(int r = 0; r < 5; r++) {
-				if(!flag2 && playfair[r][c] == arr[0]) {
-					index[0][0] = r;
-					index[0][1] = c;
-					flag1 = true;
-				}
-				else if(flag2 && playfair[r][c] == arr[0]) {
-					index[1][0] = r;
-					index[1][1] = c;
-					return true;
-				}
-				
-				if(!flag1 && playfair[r][c] == arr[0]) {
-					index[0][0] = r;
-					index[0][1] = c;
-					flag2 = true;
-				}
-				else if(flag1 && playfair[r][c] == arr[0]) {
-					index[1][0] = r;
-					index[1][1] = c;
-					return true;
-				}
-			}
-		}
-		
-		return false;
-	}
-	
+        Node() {};
+
+        Node(int r, int c) {
+            this.r = r;
+            this.c = c;
+        }
+    }
+
+    static String message, key;
+    static char[][] playfair;
+    static boolean[] alphabet;
+    static List<char[]> messages;
+    static HashMap<Character, Node> nodes;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+        message = sc.next();
+        key = sc.next();
+
+        playfair = new char[5][5];
+        alphabet = new boolean[26];
+        messages = new ArrayList<>();
+        nodes = new HashMap<>();
+
+        init();
+
+        for(int i = 0; i < messages.size(); i++) {
+            Node node1 = nodes.get(messages.get(i)[0]);
+            Node node2 = nodes.get(messages.get(i)[1]);
+
+            // 가로가 같을 때
+            if(node1.r == node2.r) {
+                sb.append(playfair[node1.r][(node1.c + 1) % 5]);
+                sb.append(playfair[node2.r][(node2.c + 1) % 5]);
+            }
+            // 세로가 같을 때
+            else if(node1.c == node2.c) {
+                sb.append(playfair[(node1.r + 1) % 5][node1.c]);
+                sb.append(playfair[(node2.r + 1) % 5][node2.c]);
+            }
+            // 다 다를 때
+            else {
+                sb.append(playfair[node1.r][node2.c]);
+                sb.append(playfair[node2.r][node1.c]);
+            }
+        }
+
+        System.out.println(sb);
+
+    }
+
+    static void init() {
+        int idx = 0;
+        o : for(int r = 0; r < 5; r++) {
+            for(int c = 0; c < 5; c++) {
+                while(alphabet[key.charAt(idx) - 'A']) {
+                    idx++;
+                    if(idx >= key.length()) break o;
+                }
+
+                alphabet[key.charAt(idx) - 'A'] = true;
+                playfair[r][c] = key.charAt(idx);
+                nodes.put(key.charAt(idx), new Node(r, c));
+            }
+        }
+
+        idx = 0;
+        o : for(int r = 0; r < 5; r++) {
+            for(int c = 0; c < 5; c++) {
+                if(playfair[r][c] != '\u0000') continue;
+
+                while(idx == 9 || alphabet[idx]) {
+                    idx++;
+                    if(idx >= 26) break o;
+                }
+                alphabet[idx] = true;
+                playfair[r][c] = (char) (idx + 'A');
+                nodes.put((char) (idx + 'A'), new Node(r, c));
+            }
+        }
+
+        idx = 0;
+        while(idx < message.length()) {
+            char[] temp = new char[2];
+            char ch = message.charAt(idx);
+            if(idx == message.length()-1) {
+                temp[0] = ch;
+                temp[1] = 'X';
+                messages.add(temp);
+                break;
+            }
+
+            if(ch == message.charAt(idx + 1)) {
+                if(ch == 'X') {
+                    temp[0] = ch;
+                    temp[1] = 'Q';
+                } else {
+                    temp[0] = ch;
+                    temp[1] = 'X';
+                }
+                idx += 1;
+            } else {
+                temp[0] = ch;
+                temp[1] = message.charAt(idx + 1);
+                idx += 2;
+            }
+
+            messages.add(temp);
+        }
+    }
 }
