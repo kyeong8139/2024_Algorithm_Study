81bca39c7748ed286da73975752a30cd018f9c40
5ooyeon <symelcho@inu.ac.kr>
2024-07-11 22:34:49 +0900

    [baekjoon] 수도배관공사

--- baekjoon/Sooyeon/수도배관공사_2073.java
+++ baekjoon/Sooyeon/수도배관공사_2073.java
@@ -5,39 +5,30 @@ import java.util.*;
  * 수도배관공사_2073
  */
 public class 수도배관공사_2073 {
-    static class Pipe {
-        int L, C;
-
-        Pipe(int L, int C) {
-            this.L = L;
-            this.C = C;
-        }
-
-    }
     static int D, P;
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
         D = Integer.parseInt(st.nextToken()); P = Integer.parseInt(st.nextToken());
-        List<Pipe> arr = new ArrayList<>();
+
+        int[] dp = new int[D + 1];
+        for (int i = 0; i <= D; i++) {
+            dp[i] = 0;
+        }
+        dp[0] = Integer.MAX_VALUE;
+
         for(int i=0;i<P;i++) {
             st = new StringTokenizer(bf.readLine());
-            arr.add(new Pipe(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
-        }
-
-        Collections.sort(arr, new Comparator<Pipe>() {
-
-            @Override
-            public int compare(Pipe o1, Pipe o2) {
-                if(o1.L == o2.L) {
-                    return 1;
+            int L = Integer.parseInt(st.nextToken()), C = Integer.parseInt(st.nextToken());
+            for(int j = D-L; j>=0;j--) {
+                if(dp[j] > 0) {
+                    dp[j+L] = Math.max(dp[j+L], Math.min(dp[j], C));
                 }
-                return o1.L - o2.L;
             }
-            
-        });
-
+        }
+        
+        System.out.println(dp[D]);
         
     }
 }
